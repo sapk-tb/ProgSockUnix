bd913be5d3d997c729ee5922758491268902de06
Antoine GIRARD <sapk@sapk.fr>
2016-12-08 11:45:03 +0100

    init

--- bus_server.c
+++ bus_server.c
@@ -11,170 +11,178 @@
 
 #define BUF_SIZE 1024
 
-int server;	/* socket du serveur en attente de connection */
-char * sockname;	/* nom de la socket */
+int server; /* socket du serveur en attente de connection */
+char * sockname; /* nom de la socket */
 
 static inline int max(int x, int y) {
-  return x > y ? x : y; 
-}  
-  
-void hdlr_fin(int sig) {
-  printf("Reception du signal %d. Arret du bus !\n", sig);
-  close(server);
-  unlink(sockname);
-  exit(EXIT_SUCCESS);
+    return x > y ? x : y;
 }
 
+void hdlr_fin(int sig) {
+    printf("Reception du signal %d. Arret du bus !\n", sig);
+    close(server);
+    unlink(sockname);
+    exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char **argv) {
-  struct sockaddr_un a;
-  int * clients;	/* sockets des clients */
-  int max_clients;	/* nombre de clients */
-  char buffer[BUF_SIZE];
-  int r, i, j;
-  ssize_t rd_sz, wr_sz;
+    struct sockaddr_un a;
+    int * clients; /* sockets des clients */
+    int max_clients; /* nombre de clients */
+    char buffer[BUF_SIZE];
+    int r, i, j;
+    ssize_t rd_sz, wr_sz;
 
-  if (argc != 3) {
-    fprintf(stderr,
-      "Utilisation:\n\t%s <Unix socket path> <max clients>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-
-  /*
-  * The SIGPIPE signal will be received if the peer has gone away
-  * and an attempt is made to write data to the peer. Ignoring this
-  * signal causes the write operation to receive an EPIPE error.
-  * Thus, the user is informed about what happened.
-  */
-  signal(SIGPIPE, SIG_IGN);
-  
-  /* pour une terminaison propre sur QUIT TERM INT SEGV... */
-  signal(SIGQUIT,  hdlr_fin);
-  signal(SIGTERM, hdlr_fin);
-  signal(SIGINT, hdlr_fin);
-  signal(SIGSEGV, hdlr_fin);
-
-  max_clients = atoi(argv[2]);
-  clients = (int *) malloc( max_clients * sizeof(int) );
-  for (i=0; i<max_clients; i++)	/* initialisation de cette structure */
-    clients[i] = -1;	/* convention choisie  -1 = pas de client */ 
-
-
-  /* creation de la socket serveur */
-  if ((server = socket(PF_UNIX, SOCK_RAW, 0)) < 0) {
-    perror("socket()");
-    exit(EXIT_FAILURE);
-  }
-
-  /* preparation de la structure d'adresse de la socket serveur sur 
-     laquelle on attend les connexions */
-  memset(&a, 0, sizeof(a));
-  a.sun_family = AF_UNIX;
-  sockname = strncpy(a.sun_path,argv[0],255);
-
-  if (bind(server, &a, sizeof(a)) < 0) {
-    perror("bind()");
-    close(server);
-    exit(EXIT_FAILURE);
-  }
-  if ( listen(?????????????????) < 0 ) {
-    perror("listen()"); 
-    close(server);
-    exit(EXIT_FAILURE);
-  }
-  
-  /* le corps du programme */
-  printf("Demarrage du bus.\n");
-  for (;;) {
-    int nfds = 0;
-    fd_set rd_set;  /* Catalogue des sockets interessantes en lecture */
-    
-    /* Nettoyage de ce catalogue */
-    FD_ZERO(&rd_set);
-
-    /* On y place les descripteurs interessants en lecture */
-    /* On s'interesse a la socket du serveur pour d'eventuelles connexions */
-    FD_SET(server, &rd_set);
-    nfds = max(nfds, server);
-    /* ajoute les sockets des clients */
-    for (i=0; i<max_clients; i++) {
-      if ( clients[i] > 0 ) {
-        FD_SET(clients[i], &rd_set);
-        nfds = max(nfds, clients[i]);
-      }
+    if (argc != 3) {
+        fprintf(stderr,
+                "Utilisation:\n\t%s <Unix socket path> <max clients>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    /* Se bloque en attente de quelque chose d'interessant sur une socket */
-    r = select(????????????????????????);
+    /*
+     * The SIGPIPE signal will be received if the peer has gone away
+     * and an attempt is made to write data to the peer. Ignoring this
+     * signal causes the write operation to receive an EPIPE error.
+     * Thus, the user is informed about what happened.
+     */
+    signal(SIGPIPE, SIG_IGN);
 
-    if (r == -1 && errno == EINTR)
-      continue;
-    if (r < 0) {
-      perror("select()");
-      exit(EXIT_FAILURE);
+    /* pour une terminaison propre sur QUIT TERM INT SEGV... */
+    signal(SIGQUIT, hdlr_fin);
+    signal(SIGTERM, hdlr_fin);
+    signal(SIGINT, hdlr_fin);
+    signal(SIGSEGV, hdlr_fin);
+
+    max_clients = atoi(argv[2]);
+    clients = (int *) malloc(max_clients * sizeof (int));
+    for (i = 0; i < max_clients; i++) /* initialisation de cette structure */
+        clients[i] = -1; /* convention choisie  -1 = pas de client */
+
+
+    /* creation de la socket serveur */
+    if ((server = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
     }
 
-    if (FD_ISSET(server, &rd_set)) {	/* on a une nouvelle connection */
-      r = accept(???????????????????????);
-      if (r < 0) {
-	perror("accept()");
-      } else {
-        /* on recherche un emplacement de libre dans notre liste de clients */
-        for (i=0; (i<max_clients) && (clients[i] > 0); i++);
-        if (i < max_clients) {
-          printf("Un nouveau client %d se connecte.\n", i);
-          /* on y ajoute la socket du client */
-          clients[i] = r;
-        }
-        else {
-          printf("Plus de place pour un nouveau client.\n");
-          close(r);
-        }
-      }
+    /* preparation de la structure d'adresse de la socket serveur sur 
+       laquelle on attend les connexions */
+    memset(&a, 0, sizeof (a));
+    a.sun_family = AF_UNIX;
+    sockname = strncpy(a.sun_path, argv[0], 108);
+
+    if (bind(server, (struct sockaddr *) &a, sizeof (a)) < 0) {
+        perror("bind()");
+        close(server);
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server, max_clients) < 0) {
+        perror("listen()");
+        close(server);
+        exit(EXIT_FAILURE);
     }
 
-    /* Traite les lectures possibles */
-    /* Pour chaque donnee lue, on la renvoie a tous les autres */
-    for (i=0; i<max_clients; i++) {
-      if ( (clients[i] > 0) && FD_ISSET(clients[i], &rd_set)) {
-        /* Hypothese : on lit le paquet d'un seul coup !
-           Prevoir une buffer assez grand */
-        rd_sz = recv(?????????????????????);
-	if (rd_sz < 0) {
-	  perror("recv()");
-	  fprintf(stderr, "...probleme avec le client %d\n",i);
-          shutdown(????????????);
-          clients[i] = ???????????;
-        } else if (rd_sz == 0) {
-          printf("Le client %d et partit.\n", i);
-          close(??????????);
-          clients[i] = ??????????;
-        } else if (rd_sz > 0) {
-          printf("Reception de %d octets du client %d : [\n", rd_sz, i);
-	  /* On ecrit sur la sortie standard */
-          write(??????????????????);
-          printf("]\n");
-          /* Envoie le paquet (en un seul coup) a tous les autres clients */
-          for (j=0; j<max_clients; j++) {
-            if ( (clients[j] > 0) && (i != j) ) {
-              wr_sz = send(??????????????????);
-              if ( wr_sz < 0 ) {
-                /* cloture du client j */
-                perror("send()");
-                fprintf(stderr, "...probleme avec le client %d\n",j);
-               ????????????????????????
-              } else if (wr_sz == 0 ) {
-                printf("Le client %d et partit.\n", j);
-                ?????????????????
-              } else 
-                printf("Envoie de %d octets au client %d.\n", wr_sz, j);
+    /* le corps du programme */
+    printf("Demarrage du bus.\n");
+    for (;;) {
+        int nfds = 0;
+        fd_set rd_set; /* Catalogue des sockets interessantes en lecture */
+        fd_set wd_set;
+        fd_set ed_set;
+
+        /* Nettoyage de ce catalogue */
+        FD_ZERO(&rd_set);
+
+        /* On y place les descripteurs interessants en lecture */
+        /* On s'interesse a la socket du serveur pour d'eventuelles connexions */
+        FD_SET(server, &rd_set);
+        nfds = max(nfds, server);
+        /* ajoute les sockets des clients */
+        for (i = 0; i < max_clients; i++) {
+            if (clients[i] > 0) {
+                FD_SET(clients[i], &rd_set);
+                nfds = max(nfds, clients[i]);
             }
-          }
         }
-      }
+        struct timeval tv;
+        /* Attends jusqu’à 5 secondes. */
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        /* Se bloque en attente de quelque chose d'interessant sur une socket */
+        r = select(nfds, &rd_set, &wd_set, &ed_set, &tv);
+
+        if (r == -1 && errno == EINTR)
+            continue;
+        if (r < 0) {
+            perror("select()");
+            exit(EXIT_FAILURE);
+        }
+
+        struct sockaddr_storage from;
+        socklen_t fromlen = sizeof from;
+
+        if (FD_ISSET(server, &rd_set)) { /* on a une nouvelle connection */
+            r = accept(r, (struct sockaddr *) &from, &fromlen);
+            if (r < 0) {
+                perror("accept()");
+            } else {
+                /* on recherche un emplacement de libre dans notre liste de clients */
+                for (i = 0; (i < max_clients) && (clients[i] > 0); i++);
+                if (i < max_clients) {
+                    printf("Un nouveau client %d se connecte.\n", i);
+                    /* on y ajoute la socket du client */
+                    clients[i] = r;
+                } else {
+                    printf("Plus de place pour un nouveau client.\n");
+                    close(r);
+                }
+            }
+        }
+
+        /* Traite les lectures possibles */
+        /* Pour chaque donnee lue, on la renvoie a tous les autres */
+        for (i = 0; i < max_clients; i++) {
+            if ((clients[i] > 0) && FD_ISSET(clients[i], &rd_set)) {
+                /* Hypothese : on lit le paquet d'un seul coup !
+                   Prevoir une buffer assez grand */
+                rd_sz = recv(clients[i], buffer, BUF_SIZE, MSG_WAITFORONE); //TODO
+                if (rd_sz < 0) {
+                    perror("recv()");
+                    fprintf(stderr, "...probleme avec le client %d\n", i);
+                    shutdown(clients[i], SHUT_RDWR);
+                    clients[i] = -1;
+                } else if (rd_sz == 0) {
+                    printf("Le client %d et partit.\n", i);
+                    close(clients[i]);
+                    clients[i] = -1;
+                } else if (rd_sz > 0) {
+                    printf("Reception de %d octets du client %d : [\n", rd_sz, i);
+                    /* On ecrit sur la sortie standard */
+                    write(STDOUT_FILENO, buffer, rd_sz);
+                    printf("]\n");
+                    /* Envoie le paquet (en un seul coup) a tous les autres clients */
+                    for (j = 0; j < max_clients; j++) {
+                        if ((clients[j] > 0) && (i != j)) {
+                            wr_sz = send(clients[j], buffer, rd_sz, MSG_DONTWAIT); //TODO
+                            if (wr_sz < 0) {
+                                /* cloture du client j */
+                                perror("send()");
+                                fprintf(stderr, "...probleme avec le client %d\n", j);
+                                shutdown(clients[j], SHUT_RDWR);
+                                clients[j] = -1;
+                            } else if (wr_sz == 0) {
+                                printf("Le client %d et partit.\n", j);
+                                close(clients[j]);
+                                clients[j] = -1;
+                            } else
+                                printf("Envoie de %d octets au client %d.\n", wr_sz, j);
+                        }
+                    }
+                }
+            }
+        }
+
     }
-    
-  }
-  /* superflu apres la boucle infinie */
-  exit(EXIT_SUCCESS);
+    /* superflu apres la boucle infinie */
+    exit(EXIT_SUCCESS);
 }
